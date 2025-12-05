777a68b3cd949752602777871c8bc798fdb9f7a5
Praham Patel <prhm1765@gmail.com>
2025-12-05 17:46:22 +0530

    Deleting docs

--- PrahamTasks/docs/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"<Unnamed>","c":"PrintHelloWorld","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"PrintHelloWorld","l":"PrintHelloWorld()","u":"%3Cinit%3E()"}];updateSearchResults();
